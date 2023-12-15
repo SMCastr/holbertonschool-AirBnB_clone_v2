79a1ec2b8d19d474f6f239aa61256a4d286bffb4
SMCastr <mariesweetycastro@gmail.com>
2023-12-15 16:42:04 -0400

    update functions route

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -23,33 +23,27 @@ Routes:
 Note: You must use the option strict_slashes=False in your route definition
 """
 
-from flask import Flask, render_template
 from models import storage
-from models.state import State
-from models.city import City
-from flask import abort
+from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_by_states():
+    """Displays an HTML page with a list of
+    all states and related cities.
+    """
+    states = storage.all("State")
+    return render_template("8-cities_by_states.html", states=states)
+
+
 @app.teardown_appcontext
 def teardown():
+    """Remove the current SQLAlchemy session."""
     storage.close()
 
 
-@app.route("/cities_by_states", strict_slashes=False)
-def cities_by_states():
-    """
-    Route handler for /cities_by_states
-    Renders an HTML page displaying the list of states and cities.
-    """
-
-    states = storage.all(State).values()
-    sorted_states = sorted(states, key=lambda x: x.name)
-
-    return render_template("8-cities_by_states.html", states=sorted_states)
-
-
 if __name__ == "__main__":
-
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0")
