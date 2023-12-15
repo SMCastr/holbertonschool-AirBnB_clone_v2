aa293165ea629fe9165a5ccd4f62e9a8dd574ccf
SMCastr <mariesweetycastro@gmail.com>
2023-12-15 16:02:12 -0400

    fix blan-space

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,13 +5,12 @@ Start a Flask web application
 The web application must be
 listening on: 0.0.0.0, port 5000
 Routes:
-    /: display “Hello HBNB!” 
+/: display “Hello HBNB!” 
 Note: You must use the option strict_slashes=False in your route definition
 """
 
 
 from flask import Flask
-
 
 
 app = Flask(__name__)
