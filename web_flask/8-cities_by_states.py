073744292ef3df705bef429f4ddfdd8996713e45
SMCastr <mariesweetycastro@gmail.com>
2023-12-15 20:27:02 -0400

    correction Out-

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,13 +1,11 @@
-#!/usr/bin/python3#!/usr/bin/python3
+#!/usr/bin/python3
 """
 Script that starts a Flask web application.
 
 The web application must be listening on:
     - Address: 0.0.0.0
     - Port: 5000
-Use storage for fetching data from the storage engine
-(FileStorage or DBStorage)
-=> from models import storage and storage.all(...)
+Use storage for fetching data from the storage engine (FileStorage or DBStorage) => from models import storage and storage.all(...)
 After each request you must remove the current SQLAlchemy Session:
     - Declare a method to handle @app.teardown_appcontext
     - Call in this method storage.close()
@@ -22,29 +20,24 @@ Routes:
         LI tag: description of one City: <city.id>: <B><city.name></B>
 Note: You must use the option strict_slashes=False in your route definition
 """
-
-from flask import Flask, render_template
 from models import storage
-from models.state import State
-from flask import abort
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/cities_by_states", strict_slashes=False)
 def cities_by_states():
-    """Displays an HTML page with a list of
-    all states and related cities."""
+    """Displays an HTML page with a list of all states and related cities."""
     states = storage.all("State")
     return render_template("8-cities_by_states.html", states=states)
 
 
 @app.teardown_appcontext
-def teardown():
+def teardown(exc):
     """Remove the current SQLAlchemy session."""
     storage.close()
 
 
 if __name__ == "__main__":
-
     app.run(host="0.0.0.0", port=5000)
