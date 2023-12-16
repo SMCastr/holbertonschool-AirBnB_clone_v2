2761722eec9f2611a50e237f85bdf98112e6e607
SMCastr <mariesweetycastro@gmail.com>
2023-12-15 20:55:34 -0400

    correct output

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -5,12 +5,13 @@ Script that starts a Flask web application.
 The web application must be listening on:
     - Address: 0.0.0.0
     - Port: 5000
-Use storage for fetching data from the storage engine (FileStorage or DBStorage)
+Use storage for fetching data from the storage engine
+(FileStorage or DBStorage)
 => from models import storage and storage.all(...)
 To load all cities of a State:
     If your storage engine is DBStorage, you must use cities relationship
     Otherwise, use the public getter method cities
-After each request you must remove the current SQLAlchemy Session:
+After each request, you must remove the current SQLAlchemy Session:
     Declare a method to handle @app.teardown_appcontext
     Call in this method storage.close()
 Routes:
@@ -42,7 +43,6 @@ app = Flask(__name__)
 @app.route("/states", strict_slashes=False)
 def states():
     """Displays an HTML page with a list of all States.
-
     States are sorted by name.
     """
     states = storage.all(State).values()
