53ef69617e495fdaf1d25917cafff5d06a91545b
SMCastr <mariesweetycastro@gmail.com>
2023-12-15 20:30:08 -0400

    fix <module>

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""
-Script that starts a Flask web application.
+""" Script that starts a Flask web application.
 
 The web application must be listening on:
     - Address: 0.0.0.0
